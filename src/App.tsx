4b11e2bdb0fb6dfa29381b5f770e95f71c1ae62d
chinook25 <chinook25@gmail.com>
2021-09-14 16:14:25 +0200

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Grid, Paper, Typography} from '@material-ui/core';
+import {AppContextProvider} from './AppContext/AppContext';
+import CityInput from './CityInput/CityInput';
+import InformationView from './InformationView/InformationView';
+import SearchButton from './SearchButton/SearchButton';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContextProvider>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Typography variant="h2">Location information finder</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="h6">Where would you like to go?</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <CityInput />
+        </Grid>
+        <Grid item xs={12}>
+          <SearchButton />
+        </Grid>
+        <Grid item xs={12} component={Paper}>
+          <InformationView />
+        </Grid>
+      </Grid>
+    </AppContextProvider>
   );
 }
-
-export default App;
